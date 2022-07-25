a7b6c10d4a22c272cd1d3195d388cabddad6774a
sojeongw <sojeongw.official@gmail.com>
2022-07-26 05:44:32 +0900

    스프링 데이터 JPA 예제: 유효한 Account 엔티티 생성

--- src/main/java/io/reflectoring/buckpal/domain/Account.java
+++ src/main/java/io/reflectoring/buckpal/domain/Account.java
@@ -1,14 +1,20 @@
 package io.reflectoring.buckpal.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.time.LocalDateTime;
 
 
-@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Account {
 
+    @Getter
     private AccountId id;
+
     private Money baselineBalance;
+
+    @Getter
     private ActivityWindow activityWindow;
 
     public Money calculateBalance() {
@@ -18,6 +24,16 @@ public class Account {
         );
     }
 
+    // 유효한 상태의 account만 생성할 수 있는 팩터리 메서드 제공
+    public static Account withoutId(Money baselineBalance, ActivityWindow activityWindow) {
+        return new Account(null, baselineBalance, activityWindow);
+    }
+
+    public static Account withId(AccountId accountId, Money baselineBalance, ActivityWindow activityWindow) {
+        return new Account(accountId, baselineBalance, activityWindow);
+    }
+
+    // 계좌 잔고 확인 등 유효성 검증을 모든 상태 변경 메서드에서 하므로 유효하지 않은 도메인 모델을 생성할 수 없다.
     public boolean withdraw(Money money, AccountId targetAccountId) {
         // 비즈니스 규칙은 도메인 엔티티 안에 넣는다.
         if (!mayWithdraw(money)) {
